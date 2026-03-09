067797b09f0776b8a52c28e92998048c644f34b3
dtsteele17 <dtsteele17@gmail.com>
2026-03-09 17:23:10 +0000

    Added next.config.js

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  outputFileTracing: true,
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+}
+
+module.exports = nextConfig
